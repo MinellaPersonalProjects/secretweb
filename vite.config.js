851db439d8d8575a5ab4d00d9f591dad0384683b
noella2001 <mogboella@gmail.com>
2023-09-16 15:47:21 -0500

    add api link

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api':{
-        target : 'http://localhost:9000',
+        target : 'https://secretweb-mrgb.onrender.com',
         changeOrigin : true
       }
 
